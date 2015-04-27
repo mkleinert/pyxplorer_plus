7d2bcef8b3496bbe60ce079c3d6c93fa381f7f0b
mkleinert <matt.kleinert@gmail.com>
2015-04-27 10:10:38 -0400

    renamed to pyxplore_plus

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='pyxplorer',
+    name='pyxplorer_plus',
     version='0.1.0',
     author='Martin Grund',
     author_email='grundprinzip+pip@gmail.com',
-    packages=['pyxplorer'],
-    url='http://github.com/grundprinzip/pyxplorer',
+    packages=['pyxplorer_plus'],
+    url='http://github.com/grundprinzip/pyxplorer_plus',
     license='LICENSE',
     description='Simple Big Data Profiling',
     long_description=open('README.rst').read(),
